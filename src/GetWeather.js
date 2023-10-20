746131f08a3dcd5f3100f980b41059f32d8bb1a4
DavidxDl <Davidbekamlopez@gmail.com>
2023-10-20 19:01:05 -0300

    change the get weather function to use https now

--- src/GetWeather.js
+++ src/GetWeather.js
@@ -3,7 +3,7 @@ export default async function GetWeather(city)
 {
     if (!city) return;
     try {
-        const response = await fetch('http://api.weatherapi.com/v1/current.json?key=cb3fb464a54f4aaf9cd32643231910&q=' + city)   
+        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=cb3fb464a54f4aaf9cd32643231910&q=' + city)   
         const data = await response.json();
         console.log(data);
         return data;
